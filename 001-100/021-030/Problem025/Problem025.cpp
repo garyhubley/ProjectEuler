eacb3f134c72cf686ed69eb5f605f9efb0ee5d9b
garyhubley <garyhubley@cartenav.com>
2018-06-29 17:27:00 -0300

    Problem 025 complete

--- 001-100/021-030/Problem025/Problem025.cpp
+++ 001-100/021-030/Problem025/Problem025.cpp
@@ -6,7 +6,7 @@
 *		This is my attempt at problem025 from projecteuler.net
 *
 * The Fibonacci sequence is defined by the recurrence relation:
-* 
+*
 *		Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
 *
 * Hence the first 12 terms will be:
@@ -31,19 +31,78 @@
 
 #include <iostream>
 #include <chrono>
+#include <algorithm>
+
+#include "EulerLib.h"
 
 typedef std::chrono::high_resolution_clock Clock;
 #define ToSeconds( x ) ( std::chrono::duration_cast<std::chrono::seconds>( x ) )
 #define ToMilliSeconds( x ) ( std::chrono::duration_cast<std::chrono::milliseconds>( x ) )
 
+LargeNumber operator+( const LargeNumber& lhs, const LargeNumber& rhs )
+{
+	LargeNumber res( std::max( lhs.size(), rhs.size() ) );
+
+	auto pred = [&res]( const LargeNumber& small, const LargeNumber& large )
+	{
+		auto sm_sz = small.size();
+		auto lg_sz = large.size();
+		uint8_t tmp = 0;
+		for ( unsigned i = 0; i < sm_sz; i++ )
+		{
+			tmp = large[i] + small[i] + tmp;
+			res[i] = tmp % 10;
+			tmp /= 10;
+		}
+		for ( unsigned i = sm_sz; i < lg_sz; i++ )
+		{
+			tmp = large[i] + tmp;
+			res[i] = tmp % 10;
+			tmp /= 10;
+		}
+		while ( tmp != 0 )
+		{
+			res.push_back( tmp % 10 );
+			tmp /= 10;
+		}
+	};
+
+	auto lhs_sz = lhs.size();
+	auto rhs_sz = rhs.size();
+	if ( lhs_sz > rhs_sz )
+	{
+		// lhs is larger
+		pred( rhs, lhs );
+	}
+	else
+	{
+		// rhs is larger
+		pred( lhs, rhs );
+	}
+
+	
+	return std::move( res );
+}
+
 int main()
 {
+	LargeNumber firstPrevious( 1, 1 ), secondPrevious( 1, 1 ), current;
+	uint32_t count = 2;
 
 	auto start = Clock::now();
 
+	while ( current.size() < 1000 )
+	{
+		count++;
+
+		current = firstPrevious + secondPrevious;
+		secondPrevious = firstPrevious;
+		firstPrevious = current;
+	}
+
 	auto end = Clock::now();
 
-	std::cout << "Answer: " << std::endl;
+	std::cout << "Answer: " << count << std::endl;
 	std::cout << "Time: " << ToMilliSeconds( end - start ).count() << " milliseconds" << std::endl;
 	std::cin.get();
 	return 0;
