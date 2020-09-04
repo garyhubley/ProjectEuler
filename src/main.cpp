20f05ae70fd5bf96d3372aaf7f9d47bda87e5f54
Gary Hubley <>
2020-09-04 13:49:33 -0300

    allow command line args

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,25 @@
 
 #include <iostream>
 
-int main() {
+int main( int argc, char **argv ) {
 
 	unsigned int problemNum;
     auto numProblems = problems.size();
 
-	std::cout << numProblems << " problems have been complete." << std::endl;
-	std::cout << "What problem would you like to see?" << std::endl;
-	std::cin >> problemNum;
-	 
+    switch( argc ) {
+        case 1:
+            std::cout << numProblems << " problems have been complete." << std::endl;
+            std::cout << "What problem would you like to see?" << std::endl;
+            std::cin >> problemNum;
+            break;
+        case 2:
+            problemNum = std::stoi( argv[1] );
+            break;
+        default:
+            std::cout << "incorrect number of args" << std::endl;
+            exit(1);
+    }
+
 	if( problemNum > problems.size() ) {
 		std::cout << "This problem hasn't been completed yet" << std::endl;
 		exit(1);
